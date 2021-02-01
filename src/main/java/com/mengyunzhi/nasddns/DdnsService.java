656daf2147d2466b2561712dbec8192f89cf88e2
panjie <3792535@qq.com>
2021-02-01 18:22:27 +0800

    获取IP地址时，增加TRY CATCH

--- src/main/java/com/mengyunzhi/nasddns/DdnsService.java
+++ src/main/java/com/mengyunzhi/nasddns/DdnsService.java
@@ -30,161 +30,168 @@ import java.util.regex.Pattern;
  */
 @Service
 public class DdnsService {
-    private static final Logger logger = LoggerFactory.getLogger(DdnsService.class);
-    /**
-     * 当前主机IP
-     */
-    private String currentHostIpaddress;
+  private static final Logger logger = LoggerFactory.getLogger(DdnsService.class);
+  /**
+   * 当前主机IP
+   */
+  private String currentHostIpaddress;
 
-    /**
-     * 最后一次请求是否成功
-     */
-    private boolean lastRequestSuccess = true;
+  /**
+   * 最后一次请求是否成功
+   */
+  private boolean lastRequestSuccess = true;
 
-    @Autowired
-    AliyunConfig aliyunConfig;
+  @Autowired
+  AliyunConfig aliyunConfig;
 
-    /**
-     * 获取主域名的所有解析记录列表
-     */
-    private DescribeDomainRecordsResponse describeDomainRecords(DescribeDomainRecordsRequest request, IAcsClient client) {
-        try {
-            this.lastRequestSuccess = true;
-            // 调用SDK发送请求
-            return client.getAcsResponse(request);
-        } catch (ClientException e) {
-            logger.warn("发生异常。请检查：1. AccessKey ID，AccessKey Secret是否正确。");
-            logger.warn("2. 网络是否是正常");
-            logger.warn("3. 是否为阿里云账户添加AliyunDNSFullAccess权限");
-            e.printStackTrace();
-            this.lastRequestSuccess = false;
-            // 发生调用错误，抛出运行时异常
-            throw new RuntimeException();
+  /**
+   * 获取主域名的所有解析记录列表
+   */
+  private DescribeDomainRecordsResponse describeDomainRecords(DescribeDomainRecordsRequest request, IAcsClient client) {
+    try {
+      this.lastRequestSuccess = true;
+      // 调用SDK发送请求
+      return client.getAcsResponse(request);
+    } catch (ClientException e) {
+      logger.warn("发生异常。请检查：1. AccessKey ID，AccessKey Secret是否正确。");
+      logger.warn("2. 网络是否是正常");
+      logger.warn("3. 是否为阿里云账户添加AliyunDNSFullAccess权限");
+      e.printStackTrace();
+      this.lastRequestSuccess = false;
+      // 发生调用错误，抛出运行时异常
+      throw new RuntimeException();
+    }
+  }
+
+  /**
+   * 获取当前主机公网IP
+   */
+  private String getCurrentHostIpaddress() {
+    // 这里使用jsonip.com第三方接口获取本地IP
+    String jsonip = "https://jsonip.com/";
+    // 接口返回结果
+    StringBuilder result = new StringBuilder();
+    BufferedReader in = null;
+    try {
+      // 使用HttpURLConnection网络请求第三方接口
+      URL url = new URL(jsonip);
+      HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+      urlConnection.setRequestMethod("GET");
+      urlConnection.connect();
+      in = new BufferedReader(new InputStreamReader(
+          urlConnection.getInputStream()));
+      String line;
+      while ((line = in.readLine()) != null) {
+        result.append(line);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      logger.error("网络错误");
+      throw new RuntimeException("网络错误");
+    }
+    // 使用finally块来关闭输入流
+    finally {
+      try {
+        if (in != null) {
+          in.close();
         }
+      } catch (Exception e2) {
+        e2.printStackTrace();
+      }
+    }
+    // 正则表达式，提取xxx.xxx.xxx.xxx，将IP地址从接口返回结果中提取出来
+    String rexp = "(\\d{1,3}\\.){3}\\d{1,3}";
+    Pattern pat = Pattern.compile(rexp);
+    Matcher mat = pat.matcher(result.toString());
+    String res = "";
+    if (mat.find()) {
+      res = mat.group();
+    }
+    return res;
+  }
+
+  /**
+   * 修改解析记录
+   */
+  private UpdateDomainRecordResponse updateDomainRecord(UpdateDomainRecordRequest request, IAcsClient client) {
+    try {
+      // 调用SDK发送请求
+      return client.getAcsResponse(request);
+    } catch (ClientException e) {
+      e.printStackTrace();
+      // 发生调用错误，抛出运行时异常
+      throw new RuntimeException();
+    }
+  }
+
+  private static void logPrint(String functionName, Object result) {
+    Gson gson = new Gson();
+    logger.info("-------------------------------" + functionName + "-------------------------------");
+    logger.info(gson.toJson(result));
+  }
+
+  @Scheduled(fixedRate = 60 * 1000)
+  public void main() {
+    // 当前主机公网IP
+    logger.info("开始获取IP地址，未发生变化则有1/10的概率进行检查更新");
+    String currentHostIpaddress;
+    try {
+      currentHostIpaddress = this.getCurrentHostIpaddress();
+    } catch (RuntimeException exception) {
+      return;
     }
 
-    /**
-     * 获取当前主机公网IP
-     */
-    private String getCurrentHostIpaddress() {
-        // 这里使用jsonip.com第三方接口获取本地IP
-        String jsonip = "https://jsonip.com/";
-        // 接口返回结果
-        StringBuilder result = new StringBuilder();
-        BufferedReader in = null;
-        try {
-            // 使用HttpURLConnection网络请求第三方接口
-            URL url = new URL(jsonip);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-            in = new BufferedReader(new InputStreamReader(
-                    urlConnection.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result.append(line);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        // 使用finally块来关闭输入流
-        finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (Exception e2) {
-                e2.printStackTrace();
-            }
-
-        }
-        // 正则表达式，提取xxx.xxx.xxx.xxx，将IP地址从接口返回结果中提取出来
-        String rexp = "(\\d{1,3}\\.){3}\\d{1,3}";
-        Pattern pat = Pattern.compile(rexp);
-        Matcher mat = pat.matcher(result.toString());
-        String res = "";
-        if (mat.find()) {
-            res = mat.group();
-        }
-        return res;
+    if (currentHostIpaddress.equals(this.currentHostIpaddress) && this.lastRequestSuccess) {
+      logger.info("IP地址为：" + currentHostIpaddress + "未发生变化");
+      return;
     }
+    this.currentHostIpaddress = currentHostIpaddress;
 
-    /**
-     * 修改解析记录
-     */
-    private UpdateDomainRecordResponse updateDomainRecord(UpdateDomainRecordRequest request, IAcsClient client) {
-        try {
-            // 调用SDK发送请求
-            return client.getAcsResponse(request);
-        } catch (ClientException e) {
-            e.printStackTrace();
-            // 发生调用错误，抛出运行时异常
-            throw new RuntimeException();
+    // 设置鉴权参数，初始化客户端
+    DefaultProfile profile = DefaultProfile.getProfile(
+        aliyunConfig.getRegion(),
+        aliyunConfig.getId(),
+        aliyunConfig.getSecret());
+    IAcsClient client = new DefaultAcsClient(profile);
+
+    this.aliyunConfig.getDomains().forEach(domain -> {
+      // 查询指定二级域名的最新解析记录
+      DescribeDomainRecordsRequest describeDomainRecordsRequest = new DescribeDomainRecordsRequest();
+      // 主域名
+      describeDomainRecordsRequest.setDomainName(domain.getName());
+      // 主机记录
+      describeDomainRecordsRequest.setRRKeyWord(domain.getRecord());
+      // 解析记录类型
+      describeDomainRecordsRequest.setType(this.aliyunConfig.getType());
+      DescribeDomainRecordsResponse describeDomainRecordsResponse = this.describeDomainRecords(describeDomainRecordsRequest, client);
+      logPrint("describeDomainRecords", describeDomainRecordsResponse);
+
+      List<DescribeDomainRecordsResponse.Record> domainRecords = describeDomainRecordsResponse.getDomainRecords();
+      // 最新的一条解析记录
+      if (domainRecords.size() != 0) {
+        DescribeDomainRecordsResponse.Record record = domainRecords.get(0);
+        // 记录ID
+        String recordId = record.getRecordId();
+        // 记录值
+        String recordsValue = record.getValue();
+
+        logger.info("-------------------------------当前主机公网IP为：" + currentHostIpaddress + "-------------------------------");
+        if (!currentHostIpaddress.equals(recordsValue)) {
+          // 修改解析记录
+          UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
+          // 主机记录
+          updateDomainRecordRequest.setRR(domain.getRecord());
+          // 记录ID
+          updateDomainRecordRequest.setRecordId(recordId);
+          // 将主机记录值改为当前主机IP
+          updateDomainRecordRequest.setValue(currentHostIpaddress);
+          // 解析记录类型
+          updateDomainRecordRequest.setType(this.aliyunConfig.getType());
+          UpdateDomainRecordResponse updateDomainRecordResponse = this.updateDomainRecord(updateDomainRecordRequest, client);
+          logPrint("updateDomainRecord", updateDomainRecordResponse);
         }
-    }
+      }
+    });
 
-    private static void logPrint(String functionName, Object result) {
-        Gson gson = new Gson();
-        logger.info("-------------------------------" + functionName + "-------------------------------");
-        logger.info(gson.toJson(result));
-    }
-
-    @Scheduled(fixedRate = 60 * 1000)
-    public void main() {
-        // 当前主机公网IP
-        logger.info("开始获取IP地址，未发生变化则有1/10的概率进行检查更新");
-        String currentHostIpaddress = this.getCurrentHostIpaddress();
-        if (currentHostIpaddress.equals(this.currentHostIpaddress) && this.lastRequestSuccess) {
-            logger.info("IP地址为：" + currentHostIpaddress + "未发生变化");
-            return;
-        }
-        this.currentHostIpaddress = currentHostIpaddress;
-
-        // 设置鉴权参数，初始化客户端
-        DefaultProfile profile = DefaultProfile.getProfile(
-                aliyunConfig.getRegion(),
-                aliyunConfig.getId(),
-                aliyunConfig.getSecret());
-        IAcsClient client = new DefaultAcsClient(profile);
-
-        this.aliyunConfig.getDomains().forEach(domain -> {
-            // 查询指定二级域名的最新解析记录
-            DescribeDomainRecordsRequest describeDomainRecordsRequest = new DescribeDomainRecordsRequest();
-            // 主域名
-            describeDomainRecordsRequest.setDomainName(domain.getName());
-            // 主机记录
-            describeDomainRecordsRequest.setRRKeyWord(domain.getRecord());
-            // 解析记录类型
-            describeDomainRecordsRequest.setType(this.aliyunConfig.getType());
-            DescribeDomainRecordsResponse describeDomainRecordsResponse = this.describeDomainRecords(describeDomainRecordsRequest, client);
-            logPrint("describeDomainRecords", describeDomainRecordsResponse);
-
-            List<DescribeDomainRecordsResponse.Record> domainRecords = describeDomainRecordsResponse.getDomainRecords();
-            // 最新的一条解析记录
-            if (domainRecords.size() != 0) {
-                DescribeDomainRecordsResponse.Record record = domainRecords.get(0);
-                // 记录ID
-                String recordId = record.getRecordId();
-                // 记录值
-                String recordsValue = record.getValue();
-
-                logger.info("-------------------------------当前主机公网IP为：" + currentHostIpaddress + "-------------------------------");
-                if (!currentHostIpaddress.equals(recordsValue)) {
-                    // 修改解析记录
-                    UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
-                    // 主机记录
-                    updateDomainRecordRequest.setRR(domain.getRecord());
-                    // 记录ID
-                    updateDomainRecordRequest.setRecordId(recordId);
-                    // 将主机记录值改为当前主机IP
-                    updateDomainRecordRequest.setValue(currentHostIpaddress);
-                    // 解析记录类型
-                    updateDomainRecordRequest.setType(this.aliyunConfig.getType());
-                    UpdateDomainRecordResponse updateDomainRecordResponse = this.updateDomainRecord(updateDomainRecordRequest, client);
-                    logPrint("updateDomainRecord", updateDomainRecordResponse);
-                }
-            }
-        });
-
-    }
+  }
 }
